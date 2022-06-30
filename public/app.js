02f1570267d9b05afd6fa0356ffce1acb17512d5
Gloria <gloriacheung812@gmail.com>
2022-06-30 17:04:54 +0000

    add logic for reset and changing winning score

--- public/app.js
+++ public/app.js
@@ -2,11 +2,22 @@ const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
+const resetButton = document.querySelector("#reset");
+const winningScoreSelect = document.querySelector("#playto");
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
+
+// helper function
+const reset = function() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+}
 
 p1Button.addEventListener("click", () => {
   if (!isGameOver) {
@@ -16,7 +27,7 @@ p1Button.addEventListener("click", () => {
     }
     p1Display.textContent = p1Score;
   }
-})
+});
 
 p2Button.addEventListener("click", () => {
   if (!isGameOver) {
@@ -26,4 +37,11 @@ p2Button.addEventListener("click", () => {
     }
     p2Display.textContent = p2Score;
   }
-})
+});
+
+winningScoreSelect.addEventListener("change", function() {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+resetButton.addEventListener("click", reset);
